0a6d9b3020c4f307d38f43637dd0700669e16e6b
Ryan McGowan <ryan@ryanmcg.com>
2013-11-05 23:33:25 -0800

    List parses on index page.

--- resources/content/index.clj
+++ resources/content/index.clj
@@ -1,8 +1,17 @@
 {:layout :ryanmcg
  :path "index.html"}
 
-(require '[hiccup.core :refer [html h]])
-(html
-  [:h1 (h "Yeah")]
-  [:pre (h (keys tags))]
-  [:h2 (str "more")])
+(require '(hiccup [core :refer [html h]]
+                  [element :refer [link-to]]))
+
+(defn truncate [cotent])
+
+(defn post [{:keys [title date] :as parse}]
+  (let [path (incise.parsers.helpers/Parse->path parse)]
+    [:li
+     [:span.post-title (link-to path title)] " "
+     [:span.date date]]))
+
+(defn parse-is-index? [parse] (= (:path parse) "index.html"))
+
+(html [:ul.posts (map post (remove parse-is-index? parses))])
