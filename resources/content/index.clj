a1479b03756e449fd4d30991e259e0ec260e9fe5
Ryan McGowan <ryan@ryanmcg.com>
2013-12-01 18:48:48 -0800

    Just list posts on index.clj.

--- resources/content/index.clj
+++ resources/content/index.clj
@@ -4,19 +4,11 @@
 (require '(hiccup [core :refer [html h]]
                   [element :refer [link-to]]))
 
-(defn truncate [cotent])
-
 (defn post [{:keys [title date] :as parse}]
   (let [path (incise.parsers.helpers/Parse->path parse)]
     [:li
      [:span.post-title (link-to path title)] " "
      [:span.date date]]))
 
-(defn parse-is-index? [parse] (= (:path parse) "index.html"))
-
-(let [[posts pages] (split-with :date (remove parse-is-index? parses))]
-  (html
-    [:h1 "Posts"]
-    [:ul.posts (map post posts)]
-    [:h1 "Pages"]
-    [:ul.pages (map post pages)]))
+(html
+  [:ul.posts (map post (filter :date parses))])
