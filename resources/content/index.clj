b09d348a97d7814eb15fd0cf96c8aa6771eac5ac
Ryan McGowan <ryan@ryanmcg.com>
2013-12-08 17:58:39 -0800

    Use Parse->path from html ns in index.clj.

--- resources/content/index.clj
+++ resources/content/index.clj
@@ -2,10 +2,11 @@
  :path "index.html"}
 
 (require '(hiccup [core :refer [html h]]
-                  [element :refer [link-to]]))
+                  [element :refer [link-to]])
+         '[incise.parsers.html :refer [Parse->path]])
 
 (defn post [{:keys [title date] :as parse}]
-  (let [path (incise.parsers.helpers/Parse->path parse)]
+  (let [path (Parse->path parse)]
     [:li
      [:span.post-title (link-to path title)] " "
      [:span.date date]]))
