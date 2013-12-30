d6865bec21b1c419c0d84599857d4e112708a903
Ryan McGowan <ryan@ryanmcg.com>
2013-12-29 20:40:22 -0500

    Format dates to be more human readable on index.

--- resources/content/index.clj
+++ resources/content/index.clj
@@ -2,13 +2,19 @@
 
 (require '(hiccup [core :refer [html h]]
                   [element :refer [link-to]])
+         '(clj-time [format :refer [unparse formatter]]
+                    [coerce :refer [from-date]])
          '[incise.parsers.html :refer [Parse->path]])
+
+(defn mformat [date]
+  (unparse (formatter "MMM dd, yyyy")
+           (from-date date)))
 
 (defn post [{:keys [title date] :as parse}]
   (let [path (Parse->path parse)]
     [:li
      [:span.post-title (link-to path title)] " "
-     [:span.date date]]))
+     [:span.date (mformat date)]]))
 
 (html
   [:ul.posts (->> parses
