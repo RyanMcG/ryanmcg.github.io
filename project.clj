9ffd5ec8eec562c7c6beeeabba82e2e3d88207ec
Ryan McGowan <ryan@ryanmcg.com>
2014-01-13 22:40:41 -0800

    Bump incise dependency to 0.1.0.

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Creative Commons Attribution-ShareAlike 3.0 Unported License"
             :url "http://creativecommons.org/licenses/by-sa/3.0/deed.en_US"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [incise "0.1.0-SNAPSHOT"]]
+                 [incise "0.1.0"]]
   :repl-options {:init-ns incise.repl}
   :main incise.core)
