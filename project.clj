a7496fa6e4fd93c73442738d329b293d5ebfae58
Ryan McGowan <ryan@ryanmcg.com>
2014-04-04 20:46:56 -0700

    Upgrade version of incise to 0.3.2.

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
             :url "http://creativecommons.org/licenses/by-sa/3.0/deed.en_US"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [incise-stefon "0.1.0"]
-                 [incise "0.3.1"]]
+                 [incise "0.3.2"]]
   :repl-options {:init-ns incise.repl}
-  :aliases {"incise" ^:pass-through-help ["run" "-m" "incise.core"]}
-  :main incise.core)
+  :aliases {"incise" ^:pass-through-help ["run" "-m" "incise.core"]})
