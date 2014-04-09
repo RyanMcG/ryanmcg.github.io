6c99c2915b0b0648f67b9435f971efba3d89e5e5
Ryan McGowan <ryan@ryanmcg.com>
2014-04-08 20:23:31 -0700

    Upgrade version of base layouts used.

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://creativecommons.org/licenses/by-sa/3.0/deed.en_US"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [incise-stefon "0.1.0"]
-                 [incise "0.3.2"]]
+                 [incise "0.3.2"]
+                 [incise-base-hiccup-layouts "0.2.0"]]
   :repl-options {:init-ns incise.repl}
   :aliases {"incise" ^:pass-through-help ["run" "-m" "incise.core"]})
