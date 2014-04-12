3693263714e565a99a837bc762675141aa353ba6
Ryan McGowan <ryan@ryanmcg.com>
2014-04-11 19:11:25 -0700

    Remove style sources.

--- project.clj
+++ project.clj
@@ -3,9 +3,7 @@
   :url "https://github.com/RyanMcG/ryanmcg.github.io"
   :license {:name "Creative Commons Attribution-ShareAlike 3.0 Unported License"
             :url "http://creativecommons.org/licenses/by-sa/3.0/deed.en_US"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [incise-stefon "0.1.0"]
-                 [incise "0.3.2"]
-                 [incise-base-hiccup-layouts "0.2.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.ryanmcg/incise-vm-layout "0.1.0"]]
   :repl-options {:init-ns incise.repl}
   :aliases {"incise" ^:pass-through-help ["run" "-m" "incise.core"]})
