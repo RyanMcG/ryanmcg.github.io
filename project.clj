fb3e6c291f7a4d84f0e5defc718a785092786bab
Ryan McGowan <ryan@ryanmcg.com>
2015-05-31 23:50:57 -0700

    Upgrade manners.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Creative Commons Attribution-ShareAlike 3.0 Unported License"
             :url "http://creativecommons.org/licenses/by-sa/3.0/deed.en_US"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [manners "0.6.0"]
+                 [manners "0.8.0"]
                  [incise "0.5.0"]
                  [com.ryanmcg/incise-vm-layout "0.5.0"]]
   :repl-options {:init-ns incise.repl}
